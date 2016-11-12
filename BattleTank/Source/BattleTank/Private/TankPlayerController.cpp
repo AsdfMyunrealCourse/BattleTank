d0bf2764f7a2a6786e28302dc199e6b10bbe7de4
Jan-PC\Jan <jan.ammon@web.de>
2016-11-12 23:41:18 +0100

    Using virtual and override

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -2,6 +2,20 @@
 
 #include "BattleTank.h"
 #include "TankPlayerController.h"
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"))
+		auto ControlledTank = GetControlledTank();
+	if(!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not posessing a tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController posessing : %s"), *ControlledTank->GetName())
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const
 {
