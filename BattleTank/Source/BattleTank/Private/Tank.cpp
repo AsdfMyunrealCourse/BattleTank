9dfd83f5906c5142c468f0bae78d08f76837c7db
Jan-PC\Jan <jan.ammon@web.de>
2016-12-28 01:23:53 +0100

    SuggestProjectileVelocity

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -4,10 +4,7 @@
 #include "Tank.h"
 
 
-void ATank::SetBarrelReference(UStaticMeshComponent * BarrelToSet)
-{	
-	TankAimingComponent->SetBarrelReference(BarrelToSet);
-}
+
 
 // Sets default values
 ATank::ATank()
@@ -42,5 +39,10 @@ void ATank::SetupPlayerInputComponent(class UInputComponent* InputComponent)
 
 void ATank::AimAt(FVector HitLocation)
 {
-	TankAimingComponent->AimAt(HitLocation);
+	TankAimingComponent->AimAt(HitLocation, LaunchSpeed);
+}
+
+void ATank::SetBarrelReference(UStaticMeshComponent * BarrelToSet)
+{
+	TankAimingComponent->SetBarrelReference(BarrelToSet);
 }
