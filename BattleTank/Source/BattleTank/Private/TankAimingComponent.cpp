48ae02e14af12c2318546d68762b80396ea348e6
Jan-PC\Jan <jan.ammon@web.de>
2017-01-07 20:02:54 +0100

    Using Clamp

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -64,7 +64,7 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
-	Barrel->Elevate(5); //TODO remove magic Number
+	Barrel->Elevate(DeltaRotator.Pitch); 
 }
 
 
